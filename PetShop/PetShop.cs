8f3573b70436f4d993e113736d8c7ae55f810a87
Capybara666 <damian.niemczyk314@gmail.com>
2024-10-29 19:22:24 +0100

    zadanie 2.1 - rozwiazanie brzydkie

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Training.DomainClasses
 {
@@ -15,6 +16,30 @@ namespace Training.DomainClasses
         public IEnumerable<Pet> AllPets()
         {
             return new ReadOnlySet<Pet>(_petsInTheStore);
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            for (int i = 0; i < this._petsInTheStore.Count - 1; i++)
+                for (int j = 0; j < this._petsInTheStore.Count - i - 1; j++)
+                    if (this._petsInTheStore[j].name.CompareTo(this._petsInTheStore[j + 1].name) > 0)
+                    {
+                        var p = this._petsInTheStore[j];
+                        this._petsInTheStore[j] = this._petsInTheStore[j + 1];
+                        this._petsInTheStore[j + 1] = p;
+                    }
+            return new ReadOnlySet<Pet>(_petsInTheStore);
+        }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            foreach (var item in _petsInTheStore)
+            {
+                if (item.species.Equals(Species.Cat))
+                {
+                    yield return item;
+                }
+            }
         }
 
         public void Add(Pet newPet)
