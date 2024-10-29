41accdcd9c57f03461b3518e14b54ad82543bd1b
Marta Szulc <114237346+MS2908@users.noreply.github.com>
2024-10-29 19:35:24 +0100

    Add files via upload

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Training.DomainClasses
 {
@@ -24,6 +25,28 @@ namespace Training.DomainClasses
                 {
                     _petsInTheStore.Add(newPet);
                 }
+        }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            var notFoundPets = new List<Pet>();
+            foreach (var cat in _petsInTheStore)
+            {
+                if (cat.species == Species.Cat)
+                {
+                    notFoundPets.Add(cat); //yield return cat;
+                }
+            }
+
+            return notFoundPets;
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            var result = new List<Pet>(_petsInTheStore);
+
+            result.Sort((pet1, pet2) => pet1.name.CompareTo(pet2.name));
+            return result;
         }
     }
 }
