61ca6574062d3c8449a1fccf50b67de74465a97c
Mikołaj Storoniak <43695539+MikoStoro@users.noreply.github.com>
2024-10-29 19:33:39 +0100

    Add files via upload

---
+++ zto24_wt18-main/PetShop/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Training.DomainClasses;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<TItem> OneAtATIme<TItem>(this IEnumerable<TItem> items)
+    {
+        foreach (var item in items)
+        {
+            yield return item;
+        }
+    }
+}
---
+++ zto24_wt18-main/PetShop/PetShop.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace Training.DomainClasses
+{
+    public class PetShop
+    {
+        private IList<Pet> _petsInTheStore;
+
+        public PetShop(IList<Pet> petsInTheStore)
+        {
+            this._petsInTheStore = petsInTheStore;
+        }
+
+        public IEnumerable<Pet> AllPets()
+        {
+            return new ReadOnlySet<Pet>(_petsInTheStore);
+        }
+
+        public void Add(Pet newPet)
+        {
+
+                if (!_petsInTheStore.Contains(newPet))
+                {
+                    _petsInTheStore.Add(newPet);
+                }
+        }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Cat)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            var result = new List<Pet>(_petsInTheStore);
+            result.Sort((x, y) => { return x.name.CompareTo(y.name); });
+            return new ReadOnlySet<Pet>(result);
+        }
+
+    }
+}
