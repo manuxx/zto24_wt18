134abf1ab84fe89e2e28395f4d701ebdbce08c2d
Mac32123 <97965268+Mac32123@users.noreply.github.com>
2024-10-29 19:34:56 +0100

    Add files via upload

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace Training.DomainClasses
 {
@@ -24,6 +25,29 @@ namespace Training.DomainClasses
                 {
                     _petsInTheStore.Add(newPet);
                 }
+        }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            List<Pet> cats = new List<Pet>(); 
+            
+            foreach (var cat in _petsInTheStore)
+            {
+
+                if (cat.species == Species.Cat)
+                {
+                    cats.Add(cat);
+                }
+            }
+
+            return new ReadOnlySet<Pet>(cats);
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            var list = new List<Pet>(_petsInTheStore);
+            list.Sort(comparison: (pet1, pet2) => { return pet1.name.CompareTo(pet2.name);});
+            return list;
         }
     }
 }
