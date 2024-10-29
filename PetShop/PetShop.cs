ab22fe6b74295e0c8ab76a8529c137b763eaf650
lukgla <52761889+lukgla@users.noreply.github.com>
2024-10-29 19:24:55 +0100

    Implemented missing methods

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Training.DomainClasses
 {
@@ -24,6 +25,21 @@ namespace Training.DomainClasses
                 {
                     _petsInTheStore.Add(newPet);
                 }
+        }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            foreach (Pet p in this._petsInTheStore)
+            {
+                if (p.species == Species.Cat) yield return p;
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {   
+            List<Pet> tmp = new List<Pet>(_petsInTheStore);
+            tmp.Sort((pet, pet1) => pet.name.CompareTo(pet1.name) );
+            return tmp.OneAtATIme();
         }
     }
 }
