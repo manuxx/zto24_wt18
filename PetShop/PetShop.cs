fc519fa8109338cd8e3cafcf487ebcfcb03ba389
Capybara666 <damian.niemczyk314@gmail.com>
2024-10-29 19:06:06 +0100

    zadanie 4 - wyniesienie ReadOnlySet do oddzielnego pliku

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -27,24 +27,4 @@ namespace Training.DomainClasses
                 }
         }
     }
-
-    public class ReadOnlySet<TItem> : IEnumerable<TItem>
-    {
-        private readonly IEnumerable<TItem> _items;
-
-        public ReadOnlySet(IEnumerable<TItem> items)
-        {
-            this._items = items;
-        }
-
-        public IEnumerator<TItem> GetEnumerator()
-        {
-            return this._items.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-    }
 }
---
+++ PetShop/ReadOnlySet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.DomainClasses
+{
+    public class ReadOnlySet<TItem> : IEnumerable<TItem>
+    {
+        private readonly IEnumerable<TItem> _items;
+
+        public ReadOnlySet(IEnumerable<TItem> items)
+        {
+            this._items = items;
+        }
+
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            return this._items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
