324af93558aa6eb8aed03386586a1332c32f419e
Capybara666 <damian.niemczyk314@gmail.com>
2024-11-05 17:59:35 +0100

    czerwone

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -41,5 +41,45 @@ namespace Training.DomainClasses
             result.Sort((pet1, pet2) => pet1.name.CompareTo(pet2.name));
             return result;
         }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
