73155d8ebc0decc66707b6f0be35d853d7fb7220
Michał Wiliński <118672945+TeferiHero@users.noreply.github.com>
2024-11-05 19:21:08 +0100

    Added Criteria

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -7,12 +7,38 @@ public static class EnumerableExtensions
 {
     public static IEnumerable<Item> AllItemsWhere<Item>(this IEnumerable<Item> items, Predicate<Item> condition)
     {
-        foreach (var item in items)   
+        return items.AllItemsWhere(new AnonymousCriteria<Item>(condition));
+    }
+
+    public static IEnumerable<Item> AllItemsWhere<Item>(this IEnumerable<Item> items, Criteria<Item> criteria)
+    {
+        foreach (var item in items)
         {
-            if (condition(item))
+            if (criteria.IsSatisfiedBy(item))
             {
                 yield return item;
             }
         }
     }
+}
+
+public class AnonymousCriteria<T>: Criteria<T>
+{
+
+    private Predicate<T> _condition;
+
+    public AnonymousCriteria(Predicate<T> condition)
+    {
+        _condition = condition;
+    }
+
+    public bool IsSatisfiedBy(T item)
+    {
+        return _condition(item);
+    }
+}
+
+public interface Criteria<T>
+{
+    bool IsSatisfiedBy(T item);
 }
