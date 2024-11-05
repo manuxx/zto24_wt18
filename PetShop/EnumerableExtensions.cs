7809800c00f3e356ad0144defb12d76f09a61127
Marta Szulc <114237346+MS2908@users.noreply.github.com>
2024-11-05 19:22:45 +0100

    Add files via upload

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -14,10 +14,33 @@ public static class EnumerableExtensions
 
     public static IEnumerable<TItem> AllPetsWhere<TItem>(this IList<TItem> pets, Predicate<TItem> condition)
     {
+        return pets.AllPetsWhere(new AnonymousCriteria<TItem>(condition));
+    }
+
+    public static IEnumerable<TItem> AllPetsWhere<TItem>(this IList<TItem> pets, Criteria<TItem> criteria)
+    {
         foreach (var pet in pets)
         {
-            if (condition(pet))
+            if (criteria.IsSatisfiedBy(pet))
                 yield return pet;
         }
     }
+}
+
+public class AnonymousCriteria<T> : Criteria<T>
+{
+    public AnonymousCriteria(Predicate<T> condition)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsSatisfiedBy(T pet)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface Criteria<TItem>
+{
+    bool IsSatisfiedBy(TItem pet);
 }
