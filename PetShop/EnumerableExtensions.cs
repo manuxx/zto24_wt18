5be55a5b5826051210309ba41e89d72adb30304f
tad1 <tadziobrzeski@gmail.com>
2024-11-05 19:21:47 +0100

    add criteria API

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -14,10 +14,35 @@ public static class EnumerableExtensions
 
     public static IEnumerable<TItem> AllItemsThat<TItem>(this IList<TItem> items, Predicate<TItem> condition)
     {
+        return AllItemsThat(items, new AnonymousCriteria<TItem>(condition));
+    }
+    
+    public static IEnumerable<TItem> AllItemsThat<TItem>(this IList<TItem> items, Criteria<TItem> criteria)
+    {
         foreach (var item in items)
         {
-            if(condition(item))
+            if(criteria.IsSatisfiedBy(item))
                 yield return item;
         }
     }
+}
+
+public class AnonymousCriteria<T> : Criteria<T>
+{
+    private readonly Predicate<T> condition;
+
+    public AnonymousCriteria(Predicate<T> condition)
+    {
+        this.condition = condition;
+    }
+
+    public bool IsSatisfiedBy(T item)
+    {
+        return condition(item);
+    }
+}
+
+public interface Criteria<TItem>
+{
+    bool IsSatisfiedBy(TItem item);
 }
