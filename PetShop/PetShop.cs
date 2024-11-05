5f15a061058275cd1b8e95995af1d17343b280b6
Teravista <48471616+Teravista@users.noreply.github.com>
2024-11-05 18:21:02 +0100

    done

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -41,5 +41,68 @@ namespace Training.DomainClasses
             result.Sort((pet1, pet2) => pet1.name.CompareTo(pet2.name));
             return result;
         }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            return MYMETHOD(pet => pet.species == Species.Mouse);
+        }
+
+        private IEnumerable<Pet> MYMETHOD(Func<Pet, bool> parametr)
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (parametr(pet))
+                    yield return pet;
+            }
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            return MYMETHOD(pet => pet.sex == Sex.Female);
+
+            
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            return MYMETHOD(pet => pet.species == Species.Cat || pet.species == Species.Dog);
+
+         
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            return MYMETHOD(pet => pet.species != Species.Mouse);
+
+           
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            return MYMETHOD(pet => pet.species == Species.Dog && pet.sex == Sex.Male);
+
+          
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            return MYMETHOD(pet => pet.yearOfBirth > 2010);
+
+          
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            return MYMETHOD(pet => pet.yearOfBirth > 2010 && pet.species == Species.Dog);
+
+
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            return MYMETHOD(pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit);
+
+            
+        }
     }
 }
