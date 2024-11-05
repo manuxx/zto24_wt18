dfb86216f2aec1147ac53bfe0502a4c7466e3b48
Krzysztof Napiórkowski <KrzysiuN@proton.me>
2024-11-05 19:19:17 +0100

    feat: wprowadzenie klasy Criteria

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -14,10 +14,34 @@ public static class EnumerableExtensions
 
     public static IEnumerable<TItem> AllItemsWhere<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
     {
+        return items.AllItemsWhere(new Criteria<TItem>(condition));
+    }
+
+    public static IEnumerable<TItem> AllItemsWhere<TItem>(this IEnumerable<TItem> items, ICriteria<TItem> condition)
+    {
         foreach (var item in items)
         {
-            if (condition(item))
+            if (condition.IsSatisfiedBy(item))
                 yield return item;
         }
     }
+}
+
+public class Criteria<T>: ICriteria<T>
+{
+    private Predicate<T> _condition;
+    public Criteria(Predicate<T> condition)
+    {
+        this._condition = condition;
+    }
+
+    public bool IsSatisfiedBy(T item)
+    {
+        return _condition(item);
+    }
+}
+
+public interface ICriteria<TItem>
+{
+    bool IsSatisfiedBy(TItem item);
 }
